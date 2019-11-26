aa4f997fa9c8888f0fc8d36a5a5d9a6ec23d2457
Robert VALENTIN <rvalenti@e2r3p5.42.fr>
2019-11-26 23:05:41 +0100

    test

--- ft_malloc.h
+++ ft_malloc.h
@@ -6,7 +6,7 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 15:32:40 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/11/26 20:49:41 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/11/26 23:05:29 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@
 # include <stdlib.h>
 # include <sys/mman.h>
 
+typedef enum	e_type
+{
+	E_TINY,
+	E_SMALL,
+	E_LARGE
+}				t_type;
+
 typedef	struct	s_zone
 {
 	int				status;
@@ -27,21 +34,12 @@ typedef	struct	s_zone
 	struct s_zone	*next;
 }				t_zone;
 
-typedef	struct	s_page
-{
-	int				type;
-	size_t			size;
-	struct s_zone	zone;
-	struct s_page	*next;
-}				t_page;
-
 typedef struct	s_env
 {
-	struct s_page	tiny;
-	struct s_page	small;
-	struct s_page	large;
-}				t_env
-
+	struct s_zone	tiny;
+	struct s_zone	small;
+	struct s_zone	large;
+}				t_env;
 
 void			free(void *ptr);
 void			*malloc(size_t size);
--- malloc.c
+++ malloc.c
@@ -6,61 +6,15 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 15:54:08 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/11/26 21:25:18 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/11/26 22:59:52 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
-
-static void check_empty(t_page *page, size_t size)
-{
-	t_page *tmpage;
-	t_zone *tmzone;
-
-	tmpage = page;
-	while (tmpage)
-	{
-		tmzone = tmpage->zone;
-		while (tmzone)
-		{
-			if (tmpzone->size >= size)
-				break ;
-			tmzone = tmzone->next;
-		}
-		tmppage = tmpage->next;
-	}
-
-}
-
-static void creat_zone(size_t size ,t_env env)
-{
-	t_zone zone;
-
-	zone.status = 1;
-	zone.size = size;
-	zone.next = NULL;
-	
-}
-
-static void creat_page(size_t size,t_env env, int type)
-{
-	void	*addr;
-	t_page	page;
-
-	addr = mmap(0, size,PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-	page.type = type;
-	page.size = size;
-	page.zone = NULL;
-	page.next = NULL;
-	ft_memcpy(addr, &page, sizeof(t_page));
-	env->tiny = addr;
-}
 
 void	*malloc(size_t size)
 {
 	static t_env env;
 	if (size <= 0)
 		return (NULL);
-	creat_page(size * 100, env, 1);
-	return 	
 }
