412e00073ca1999635e064a491fbee3a165945b7
Robert VALENTIN <rvalenti@e1r3p4.42.fr>
2019-11-26 21:25:32 +0100

    save

--- ft_malloc.h
+++ ft_malloc.h
@@ -6,21 +6,44 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 15:32:40 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/11/24 21:01:45 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/11/26 20:49:41 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MALLOC_H
 # define FT_MALLOC_H
 
-# define TINY 64
-# define SMALL 128
-# define LARGE 1024
+# define TINY 512
+# define SMALL 1024
+# define LARGE 4096
 
 # include <stdlib.h>
 # include <sys/mman.h>
 
-void	free(void *ptr);
-void	*malloc(size_t size);
-void	*realloc(void *ptr, size_t size);
+typedef	struct	s_zone
+{
+	int				status;
+	size_t			size;
+	struct s_zone	*next;
+}				t_zone;
+
+typedef	struct	s_page
+{
+	int				type;
+	size_t			size;
+	struct s_zone	zone;
+	struct s_page	*next;
+}				t_page;
+
+typedef struct	s_env
+{
+	struct s_page	tiny;
+	struct s_page	small;
+	struct s_page	large;
+}				t_env
+
+
+void			free(void *ptr);
+void			*malloc(size_t size);
+void			*realloc(void *ptr, size_t size);
 #endif
--- test.c
+++ test.c
@@ -13,7 +13,7 @@ printf("pagesize = %ld\n", sysconf(_SC_PAGE_SIZE));
 	prot = PROT_READ | PROT_WRITE | PROT_EXEC;
 	flags = MAP_ANON | MAP_PRIVATE;
 
-	test = mmap((void*)(length*(1 << 20)),length,prot,flags,0,0);
+	test = mmap(0,length,prot,flags,0,0);
 	strcpy((char*)test, "hello world\n");
 	printf("%s",(char*)test);
 }
