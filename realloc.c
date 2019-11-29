31d5b5d8e25fc551af5700d96f0eb05e8fd897c3
Robert VALENTIN <rvalenti@e2r9p2.42.fr>
2019-11-29 11:16:16 +0100

    realloc begin

--- realloc.c
+++ realloc.c
@@ -6,17 +6,30 @@
 /*   By: rvalenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 15:54:23 by rvalenti          #+#    #+#             */
-/*   Updated: 2019/11/29 10:49:50 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/11/29 11:15:47 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 
+void	*get_new_alloc(t_zone zone, size_t size)
+{
+	
+}
+
 void	*realloc(void *ptr, size_t size)
 {
-	if(ptr == NULL)
+	t_zone	*zone;
+	void	*addr;
+
+	zone = ptr - sizeof(t_zone);
+
+	if (ptr == NULL)
 		return(malloc(size));
-	if(ptr && size == 0)
+	if (ptr && size == 0)
 		free(ptr);
-	return(ptr);
+	if (zone->size >= size)
+		return (NULL);
+	addr = get_new_alloc(zone, size);
+	return (addr);
 }
